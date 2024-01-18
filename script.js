b27ecb4f10b7ab61b16052bb4cfabc3cca5866b4
omargonz01 <139013469+omargonz01@users.noreply.github.com>
2024-01-18 12:10:05 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select HTML elements
+
+const grid = document.querySelector('.grid');
+const stackBtn = document.querySelector('.stack');
+const scoreCounter = document.querySelector('.score-counter');
+const endGameScreen = document.querySelector('.end-game-screen');
+const endGameText = document.querySelector('.end-game-text');
+const playAgainBtn = document.querySelector('.play-again');
+
